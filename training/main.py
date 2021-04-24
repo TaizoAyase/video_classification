654add4a70570a5415102d5f60380fd0ab4111a1
TaizoAyase <zxcvbnmkind@gmail.com>
2021-04-24 16:42:57 +0900

    add main

--- training/main.py
+++ training/main.py
@@ -1,29 +1,19 @@
 import argparse
-import os
-import random
-import shutil
-import time
-import warnings
+from pathlib import Path
 
 import numpy as np
 import pytorch_pfn_extras as ppe
 import pytorch_pfn_extras.training.extensions as extensions
 import torch
-import torch.backends.cudnn as cudnn
-import torch.distributed as dist
-import torch.multiprocessing as mp
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.nn.parallel
 import torch.optim
-import torch.optim as optim
 import torch.utils.data
 import torch.utils.data.distributed
-import torchvision.datasets as datasets
 import torchvision.models as models
 import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
-from torchvision import datasets, transforms
+from torchvision import datasets
 from video_classification.dataset.image_dataset import ImageFolderDatasetPreload
 
 model_names = sorted(
@@ -52,7 +42,7 @@ parser.add_argument(
     help="number of data loading workers (default: 4)",
 )
 parser.add_argument(
-    "--epoch", default=90, type=int, metavar="N", help="number of total epochs to run"
+    "--epochs", default=90, type=int, metavar="N", help="number of total epochs to run"
 )
 parser.add_argument(
     "-b",
@@ -87,24 +77,31 @@ parser.add_argument(
     "--pretrained", dest="pretrained", action="store_true", help="use pre-trained model"
 )
 parser.add_argument(
-    "--seed", default=None, type=int, help="seed for initializing training. "
+    "--preload", dest="preload", action="store_true", help="pre-load dataset images"
 )
-parser.add_argument("--gpu", default=None, type=int, help="GPU id to use.")
+parser.add_argument("--seed", default=777, type=int, help="seed for splitting.")
+parser.add_argument("--gpu", default=-1, type=int, help="GPU id to use.")
+parser.add_argument("--out", default="./results", type=str, help="Output dirname.")
+parser.add_argument("--snapshot", default=None, type=str, help="Snapshot to resume")
 
 
-def train(manager, args, model, device, train_loader):
+def train(manager, args, model, lossfn, device, train_loader):
     while not manager.stop_trigger:
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
+            correct = 0
             with manager.run_iteration(step_optimizers=["main"]):
                 data, target = data.to(device), target.to(device)
                 output = model(data)
-                loss = F.nll_loss(output, target)
+                loss = lossfn(output, target)
                 ppe.reporting.report({"train/loss": loss.item()})
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+                ppe.reporting.report({"val/acc": correct / len(data)})
                 loss.backward()
 
 
-def test(args, model, device, data, target):
+def test(args, model, lossfn, device, data, target):
     """The extension loops over the iterator in order to
     drive the evaluator progress bar and reporting
     averages
@@ -115,7 +112,7 @@ def test(args, model, device, data, target):
     data, target = data.to(device), target.to(device)
     output = model(data)
     # Final result will be average of averages of the same size
-    test_loss += F.nll_loss(output, target, reduction="mean").item()
+    test_loss += lossfn(output, target, reduction="mean").item()
     ppe.reporting.report({"val/loss": test_loss})
     pred = output.argmax(dim=1, keepdim=True)
     correct += pred.eq(target.view_as(pred)).sum().item()
@@ -125,8 +122,8 @@ def test(args, model, device, data, target):
 def main():
     args = parser.parse_args()
 
-    use_cuda = args.cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
+    use_cuda = args.gpu >= 0 and torch.cuda.is_available()
+    device = torch.device(f"cuda:{args.gpu}" if use_cuda else "cpu")
 
     mean = np.loadtxt("../data/mean.txt")
     sigma = np.loadtxt("../data/sigma.txt")
@@ -141,10 +138,17 @@ def main():
             normalize,
         ]
     )
-    dataset = ImageFolderDatasetPreload(args.data, transform_fn)
+    if args.preload:
+        dataset = ImageFolderDatasetPreload(args.data, transform_fn)
+    else:
+        dataset = datasets.ImageFolder(args.data, transform_fn)
 
     train_indices, val_indices = train_test_split(
-        list(range(len(dataset.targets))), test_size=0.2, stratify=dataset.targets
+        list(range(len(dataset.targets))),
+        test_size=0.2,
+        random_state=args.seed,
+        shuffle=True,
+        stratify=dataset.targets,
     )
     train_dataset = torch.utils.data.Subset(dataset, train_indices)
     val_dataset = torch.utils.data.Subset(dataset, val_indices)
@@ -157,3 +161,69 @@ def main():
     val_data_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=args.batch_size, shuffle=True, **kwargs
     )
+
+    model = models.__dict__[args.arch]()
+    model.to(device)
+
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        args.lr,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+    )
+
+    lossfn = nn.CrossEntropyLoss().to(device)
+
+    outdir = Path(args.out)
+    outdir.mkdir(exist_ok=True)
+    my_extensions = [
+        extensions.LogReport(),
+        extensions.ProgressBar(),
+        extensions.observe_lr(optimizer=optimizer),
+        extensions.ParameterStatistics(model, prefix="model"),
+        extensions.VariableStatisticsPlot(model),
+        extensions.Evaluator(
+            val_data_loader,
+            model,
+            eval_func=lambda data, target: test(
+                args, model, lossfn, device, data, target
+            ),
+            progress_bar=True,
+        ),
+        extensions.PlotReport(["train/loss", "val/loss"], "epoch", filename="loss.png"),
+        extensions.PlotReport(
+            ["train/acc", "val/acc"], "epoch", filename="accuracy.png"
+        ),
+        extensions.PrintReport(
+            [
+                "epoch",
+                "iteration",
+                "train/loss",
+                "train/acc",
+                "val/loss",
+                "val/acc",
+                "lr",
+            ]
+        ),
+        extensions.snapshot(),
+    ]
+    trigger = None
+    manager = ppe.training.ExtensionsManager(
+        model,
+        optimizer,
+        args.epochs,
+        out_dir=str(outdir),
+        extensions=my_extensions,
+        iters_per_epoch=len(train_data_loader),
+        stop_trigger=trigger,
+    )
+    # Lets load the snapshot
+    if args.snapshot is not None:
+        state = torch.load(args.snapshot)
+        manager.load_state_dict(state)
+    train(manager, args, model, lossfn, device, train_data_loader)
+    torch.save(model.state_dict(), str(outdir / "model.pt"))
+
+
+if __name__ == "__main__":
+    main()
